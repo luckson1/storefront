e30a36060d855131fd68d54d84ded142a7dac296
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-06-04 00:03:38 +0300

    feat: customisation

--- src/modules/checkout/components/payment-container/index.tsx
+++ src/modules/checkout/components/payment-container/index.tsx
@@ -26,8 +26,8 @@ const PaymentInfoMap: Record<string, { title: string; description: string }> = {
     description: "Secure payment with PayPal",
   },
   manual: {
-    title: "Test payment",
-    description: "Test payment using medusa-payment-manual",
+    title: "Payment on Delivery",
+    description: "Payment on Delivery, using M-Pesa or cash",
   },
 }
 
--- src/modules/checkout/components/payment-test/index.tsx
+++ src/modules/checkout/components/payment-test/index.tsx
@@ -6,9 +6,17 @@ const PaymentTest = () => {
       <div className="flex items-center gap-x-2 bg-yellow-100 w-full p-2">
         <Alert size={16} className="text-yellow-700" />
         <span className="text-small-regular text-yellow-700">
-          <span className="font-semibold">Attention:</span> For testing purposes
-          only.
+          <span className="font-semibold">Payment on Delivery:</span> You will be asked to pay before collecting the product. 
         </span>
+      </div>
+      <div className="flex flex-col gap-y-2">
+        <p className="text-lg text-slate-700 font-semibold">You will pay to the following till number:</p>
+     <span className="text-sm">
+     <span className="font-semibold ">Till Number : </span> 5847963
+     </span>
+     <span className="text-sm">
+        <span className="font-semibold">Name: </span> Excel Collection
+</span>
       </div>
     </div>
   )
