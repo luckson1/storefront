98de10dfc4b8b238fde79e71b21ac0ca0e3eb478
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-06-09 23:03:58 +0300

    feat: facebook verification

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,7 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="preconnect" href={`//${hostname}`} crossOrigin="true" />
+          <meta name="facebook-domain-verification" content="0ubtg0t1blcdb3bus1ld4ts29ffwvl" />
           <link rel="dns-prefetch" href={`//${hostname}`} />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap"
