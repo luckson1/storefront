779fdb3eebb33533e41d2405875bc879e94c2bd5
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-06-11 00:51:12 +0300

    fix: debug window object undefined

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,7 +15,7 @@ function App({
   pageProps,
 }: AppPropsWithLayout<{ dehydratedState?: unknown }>) {
   useEffect(() => {
-    initFacebookPixel();
+    if (typeof window !== 'undefined') { initFacebookPixel();}
   }, []);
   const getLayout = Component.getLayout ?? ((page) => page)
 
